8d628c0dcd4154432df0d65801cc1b9db6bdd405
Zak <zakary.howard94@gmail.com>
2018-12-27 21:55:38 +0100

    button now working on click and clicked when hitting the enter key

--- app.js
+++ app.js
@@ -8,19 +8,23 @@ var UIController = (function() {
 
 var controller = (function(budgetCtrl, UICtrl) {
 
-    document.querySelector('.add__btn').addEventListener('click', function() {
+    var ctrlAddItem = function() {
 
         // 1. Get field input data.
         // 2. Add item to budget controller.
         // 3. Add item to UI.
         // 4. Calculate the budget.
         // 5. Display budget on the UI.
+        console.log('It works')
+    }
 
-    });
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
 
-        console.log(event);
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
 
     });
 
