571458de5bddb636dae4d50f9ec055d22758610b
Zak <zakary.howard94@gmail.com>
2018-12-28 17:02:17 +0100

    removed package lock, no need seeing as I have yarn lock. Inputs being recognized and stored as an object, queryselector strings moved to variables

--- app.js
+++ app.js
@@ -4,21 +4,48 @@ var budgetController = (function() {
 
 var UIController = (function() {
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+
+
+    }
+
+    return {
+        getInput: (function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // inc or exp - see html
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        }),
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function() {
 
         // 1. Get field input data.
+        var input = UICtrl.getInput;
+        console.log(input);
+
         // 2. Add item to budget controller.
         // 3. Add item to UI.
         // 4. Calculate the budget.
         // 5. Display budget on the UI.
-        console.log('It works')
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
 
