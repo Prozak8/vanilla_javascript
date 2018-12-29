7c33fe176b6c7b62817f873c767b1e83e795f10f
Zak <zakary.howard94@gmail.com>
2018-12-29 15:34:25 +0100

    expense and income data types saved into data structure on button click. budgetcontroller.testing to see array of saved data. ID method to assign to saved items

--- app.js
+++ app.js
@@ -22,6 +22,35 @@ var budgetController = (function() {
             inc: 0
         }
     }
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            // Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // Push to data structure
+            data.allItems[type].push(newItem);
+
+            // Return new element
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    };
 })();
 
 var UIController = (function() {
@@ -31,8 +60,6 @@ var UIController = (function() {
         inputDescription: '.add__description',
         inputValue: '.add__value',
         inputBtn: '.add__btn',
-
-
     }
 
     return {
@@ -64,13 +91,15 @@ var controller = (function(budgetCtrl, UICtrl) {
     };
 
     var ctrlAddItem = function() {
+        var input, newItem;
 
         // 1. Get field input data.
-        var input = UICtrl.getInput;
-        console.log(input);
+        input = UICtrl.getInput();
 
         // 2. Add item to budget controller.
-        // 3. Add item to UI.
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
+        // 3. Add item to UI. 
         // 4. Calculate the budget.
         // 5. Display budget on the UI.
     };
